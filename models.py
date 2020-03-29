d2e74d1c3863d4ff4e0828f2b06a0680fb10157b
Hrafnir <g@tgg.su>
2020-03-29 14:20:52 +0300

    Participant model ready

--- models.py
+++ models.py
@@ -4,16 +4,27 @@ db = SQLAlchemy()
 
 
 class Event(db.Model):
-    __tablename__ = 'books'
-    id = db.Column(db.Integer, primary_key=True)
+    __tablename__ = 'events'
+    e_id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String)
-    date = db.Column(db.DateTime)
-    time = db.Column(db.DateTime)
-    type = db.Column(db.String)
-    category = db.Column(db.String)
+    date = db.Column(db.DateTime, nullable=False)
+    time = db.Column(db.DateTime, nullable=False)
+    type = db.Column(db.String, nullable=False)
+    category = db.Column(db.String, nullable=False)
     location = db.relationship('Location', back_populates="events")
-    address = db.Column(db.String)
-    seats = db.Column(db.Integer)
+    address = db.Column(db.String, nullable=False)
+    seats = db.Column(db.Integer, nullable=False)
     participants = db.relationship('Participant', back_populates="events")
 
+
+class Participant(db.Model):
+    __tablename__ = 'participants'
+    p_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, nullable=False)
+    password = db.Column(db.String, nullable=False)
+    picture = db.Column(db.String)
+    location = db.Column(db.String)
+    enrollmants = db.relationship('Enrollment', back_populates="participant")
+    about = db.Column(db.String)
 
