59beaaa6acff728f394fb458092a5f95b6d89d38
Hrafnir <g@tgg.su>
2020-03-30 17:14:24 +0300

    urls in progress

--- views.py
+++ views.py
@@ -1,42 +1,90 @@
-# from __init__ import app
-# from models import *
-# from flask import abort, flash, render_template, request, redirect, session, jsonify
-# from flask_jwt_extended import JWTManager, create_access_token
-#
-# jwt = JWTManager(app)
-#
-#
-# @app.route('/auth/', methods=['POST'])
-# def login():
-#     pass
-#     # if not request.is_json:
-#     #     return jsonify({"msg": "Missing JSON in request"}), 400
-#     #
-#     # username = request.json.get("username", "")
-#     # password = request.json.get("password", "")
-#     #
-#     # if username != "test" or password != "test":
-#     #     return jsonify({"msg": "Bad username or password"}), 401
-#     #
-#     # access_token = create_access_token(identity=username)
-#     # return jsonify(access_token=access_token)
-#
-#
-# @app.route("/locations/", methods=["GET"])
-# def get_locations_list():
-#     pass
-#
-#
-# @app.route("/events/", methods=["GET"])
-# def get_locations_list():
-#     pass
-#
-#
-# @app.route("/enrollments/<int:eventid>", methods=["POST", "DELETE"])
-# def enrollments(eventid):
-#     if request.method == 'POST':
-#         pass
-#     pass
+from __init__ import app
+from models import *
+from flask import abort, flash, render_template, request, redirect, session, jsonify
+from flask_jwt_extended import JWTManager, create_access_token
+
+jwt = JWTManager(app)
+
+
+@app.route('/auth/', methods=['POST'])
+def login():
+    pass
+    # if not request.is_json:
+    #     return jsonify({"msg": "Missing JSON in request"}), 400
+    #
+    # username = request.json.get("username", "")
+    # password = request.json.get("password", "")
+    #
+    # if username != "test" or password != "test":
+    #     return jsonify({"msg": "Bad username or password"}), 401
+    #
+    # access_token = create_access_token(identity=username)
+    # return jsonify(access_token=access_token)
+
+
+@app.route("/locations/", methods=["GET"])
+def get_locations_list():
+    locs = db.session.query(Location).all()
+    locs_dict = []
+    for loc in locs:
+        locs_dict.append(dict(id=loc.l_id, title=loc.title, code=loc.code))
+    return jsonify(locs_dict)
+
+
+@app.route("/events/", methods=["GET"])
+def get_events_list():
+    event_type = request.args.get('eventtype')
+    location = request.args.get('location')
+    print(event_type)
+
+    events = db.session.query(Event).all()
+    events_typed = db.session.query(Event).filter(Event.type == event_type).all()
+    events_locs = db.session.query(Event).filter(Event.loc_id == location).all()
+    events_dict = []
+    if event_type and not location:
+        print(hasattr(Event, event_type))
+        if hasattr(Event, event_type):
+            events = events.order_by(getattr(Event, event_type))
+        else:
+            return jsonify(), 500
+    elif location and not event_type:
+        print(2)
+        if hasattr(Event, location):
+            events = events.order_by(db.and_(getattr(Event, location), getattr(Event, event_type)))
+        else:
+            return jsonify(), 500
+    elif location and event_type:
+        print(3)
+        if hasattr(Event, location) and hasattr(Event, event_type):
+            events = events.order_by(getattr(Event, event_type))
+        else:
+            return jsonify(), 500
+    for e in events:
+        events_dict.append(dict(id=e.e_id,
+                                title=e.title,
+                                description=e.description,
+                                date=str(e.datetime).split(' ')[0],
+                                time=str(e.datetime).split(' ')[1],
+                                type=e.type,
+                                category=e.category,
+                                address=e.address,
+                                seats=e.seats,
+                                location=db.session.query(Location).get(e.loc_id).title
+                                ))
+    return jsonify(events_dict)
+
+
+@app.route("/enrollments/<int:eventid>", methods=["POST", "DELETE"])
+def enrollments(eventid):
+    if request.method == 'POST':
+        enroll = db.session.query(Enrollment).filter(Enrollment.event_id == eventid).first()
+        event = db.session.query(Event).get(eventid)
+        if not enroll or len(enroll) > event.seats:
+            return jsonify(status='success')
+        else:
+            return jsonify(status='error')
+
+    pass
 #
 #
 # @app.route('/register/', methods=['POST'])
