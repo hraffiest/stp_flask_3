7e2d13e5c9fc13b332abda39e15880a8e7648775
Hrafnir <g@tgg.su>
2020-04-03 20:21:03 +0300

    add stats

--- admin.py
+++ admin.py
@@ -1,7 +1,6 @@
-from flask_admin import Admin
+from flask_admin import Admin, BaseView, AdminIndexView, expose
 from flask_admin.contrib.sqla import ModelView
-
-admin = Admin()
+from models import *
 
 
 class NewModel(ModelView):
@@ -10,6 +9,22 @@ class NewModel(ModelView):
     can_delete = False
 
 
+class StatsView(BaseView):
+    @expose('/')
+    def index(self):
+        return self.render('admin/stats/index.html')
+
+
+class DashboardView(AdminIndexView):
+
+    @expose('/')
+    def index(self):
+        return self.render('admin/dashboard_index.html')
+
+
+admin = Admin(index_view=DashboardView())
+
+admin.add_view(StatsView(name='Статистика', endpoint='stats'))
 admin.add_view(NewModel(Location, db.session))
 admin.add_view(NewModel(Enrollment, db.session))
 admin.add_view(NewModel(Participant, db.session))
