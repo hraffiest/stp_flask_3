f840133c7be0146f4948944728b485cd4c1b2d71
Hrafnir <g@tgg.su>
2020-04-03 20:53:28 +0300

    ParticipantView doesent fly

--- admin.py
+++ admin.py
@@ -9,10 +9,31 @@ class NewModel(ModelView):
     can_delete = False
 
 
+class MailerView(BaseView):
+    @expose('/')
+    def mails(self):
+        return self.render('admin/mailer/index.html')
+
+
 class StatsView(BaseView):
     @expose('/')
-    def index(self):
-        return self.render('admin/stats/index.html')
+    def stats(self):
+        part = len(db.session.query(Participant).all())
+        regs = len(db.session.query(Enrollment).all())
+        return self.render('admin/stats/index.html', parts=part, regs=regs)
+
+
+# class ParticipantView(BaseView):
+#     @expose('/<e_id>')
+#     def event_stats(self, e_id):
+#         event = db.session.query(Event).get(e_id)
+#         enroll = db.session.query(Enrollment).filter(Enrollment.event_id == e_id).all()
+#
+#         if not enroll or len(enroll) > event.seats:
+#             seats = f'Осталось {len(enroll) - event.seats}'
+#         else:
+#             seats = 'Мест нет'
+#         return self.render('admin/events/index.html', event=event, seats=seats)
 
 
 class DashboardView(AdminIndexView):
@@ -24,7 +45,9 @@ class DashboardView(AdminIndexView):
 
 admin = Admin(index_view=DashboardView())
 
+admin.add_view(ParticipantView(name='ИНфа об эвентах', endpoint='events'))
 admin.add_view(StatsView(name='Статистика', endpoint='stats'))
+admin.add_view(MailerView(name='Рассылки', endpoint='mailer'))
 admin.add_view(NewModel(Location, db.session))
 admin.add_view(NewModel(Enrollment, db.session))
 admin.add_view(NewModel(Participant, db.session))
